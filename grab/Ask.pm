34491b9859859922288a1708da385eb7d5d7293b
Ed Avis <ed@membled.com>
2002-02-28 18:03:33 +0000

    *Started* to implement --configure for tv_grab_uk.  This means moving the existing 'main program' into a grab() subroutine and then calling either grab() or configure() depending on the command line.  The --configure option doesn't work yet, but tv_grab_uk is no worse than it was before and passes the test suite (parallel_test), so now is a good interim time to commit before I mess it up further.
    Made the askMyQuestion() routine in XMLTV::Ask notice when there are a
    long-winded set of options and present them as multiple choice with
    numbers.  So you can use this single routine for both short and long
    questions.

--- grab/Ask.pm
+++ grab/Ask.pm
@@ -3,9 +3,11 @@
 package XMLTV::Ask;
 use strict;
 use base 'Exporter';
-use vars '@EXPORT'; @EXPORT = qw(ask askMyQuestion askQuestion askQuestionWithoutValidation);
+use vars '@EXPORT';
+@EXPORT = qw(ask askMyQuestion askQuestion
+	     askQuestionWithoutValidation askBooleanQuestion);
 
-sub ask($)
+sub ask( $ )
 {
     print "$_[0]";
     my $result=<>;
@@ -13,45 +15,99 @@ sub ask($)
     return($result);
 }
 
-sub askMyQuestion
+# Ask a question where the answer is one of a set of alternatives.
+#
+# Parameters:
+#   question text
+#   default choice
+#   whether to validate the user's choice
+#   Remaining arguments are the choices available.
+#
+sub askMyQuestion( $$$@ )
 {
     my $question=shift(@_);
     my $default=shift(@_);
     my $validate=shift(@_);
     my @options=@_;
 
-    my $str="$question [".join(',',@options)." (default=$default)] ";
-
-    while ( 1 ) {
-	my $res=ask($str);
-	if ( !defined($res) || $res eq "" ) {
-	    return($default);
+    my $options_size = length("@options");
+    if ($options_size < 10 or not $validate) {
+	# Simple style, one line question.
+	my $str;
+	if ((my $tmp = join('', @options)) =~ tr/0-9//c) {
+	    $str="$question [".join(',',@options)." (default=$default)] ";
 	}
-	for my $val (@options) {
-	    if ( $val=~m/$res/i ) {
-		return($val);
+	else {
+	    # Just numbers, don't need to list them all.
+	    $str="$question (default=$default) ";
+	}
+	
+	while ( 1 ) {
+	    my $res=ask($str);
+	    if ( !defined($res) || $res eq "" ) {
+		return($default);
 	    }
+	    for my $val (@options) {
+		if ( $val=~m/$res/i ) {
+		    return($val);
+		}
+	    }
+	    if ( !$validate ) {
+		return($res);
+	    }
+	    print STDERR "invalid response, please choose one of ".join(',', @options)."\n";
+	    print STDERR "\n";
 	}
-	if ( !$validate ) {
-	    return($res);
+    }
+    else {
+	# Long list of options, present as numbered multiple choice.
+	die if not $validate;
+	print "$question\n";
+	my $optnum = 0;
+	my (%num_to_choice, %choice_to_num);
+	foreach (@options) {
+	    print "$optnum: $_";
+	    $num_to_choice{$optnum} = $_;
+	    $choice_to_num{$_} = $optnum;
+	    ++ $optnum;
 	}
-	print STDERR "invalid response, please choose one of ".join(',', @options)."\n";
-	print STDERR "\n";
+	my $r = askQuestion('Select one:', $choice_to_num{$default}, 0 .. $optnum);
+	return $num_to_choice{$r};
     }
 }
 
-sub askQuestion
+# Ask question with validation.
+sub askQuestion( $$@ )
 {
     my $question=shift(@_);
     my $default=shift(@_);
     return(askMyQuestion($question, $default, 1, @_));
 }
 
-sub askQuestionWithoutValidation
+sub askQuestionWithoutValidation( $$@ )
 {
     my $question=shift(@_);
     my $default=shift(@_);
     return(askMyQuestion($question, $default, 0, @_));
+}
+
+# Ask a yes/no question.
+#
+# Parameters: question text,
+#             default (true or false)
+#
+# Returns true or false.
+#
+sub askBooleanQuestion( $$ ) {
+    my ($text, $default) = @_;
+    my $r = askQuestion($text, ($default ? 'yes' : 'no'), 'yes', 'no');
+    if ($r eq 'yes') {
+	return 1;
+    }
+    elsif ($r eq 'no') {
+	return 0;
+    }
+    else { die }
 }
 
 1;
