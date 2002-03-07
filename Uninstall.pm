9c0f7beafa68bd540590fa408295de461bfafe07
Ed Avis <ed@membled.com>
2002-03-07 11:53:53 +0000

    Hacked together an 'uninstall' target.  It removes files created by 'make install'.  However it does not remove empty directories, and leaves the packlist and perllocal.pod files alone.  (These files are _modified_ in strange ways on installation, it's hard to unmodify them.)

---
+++ Uninstall.pm
@@ -0,0 +1,50 @@
+# Supplement to ExtUtils::MakeMaker to add back some rudimentary
+# uninstall functionality.  This needs to be called with an extra
+# 'uninstall' target in the Makefile, for which you will need to
+# modify your Makefile.PL.  I kept well away from the deprecated and
+# no-longer-working uninstall stuff in MakeMaker itself.
+#
+package Uninstall;
+use strict;
+use base 'Exporter'; use vars '@EXPORT'; @EXPORT = qw(uninstall);
+use File::Find;
+
+sub uninstall( % ) {
+    my %h = @_;
+    my %seen_pl;
+    foreach (qw(read write)) {
+	my $pl = delete $h{$_};
+	if (defined $pl and not $seen_pl{$pl}++) {
+	    warn "ignoring packlist $pl\n";
+	}
+    }
+    foreach my $from (keys %h) {
+	next if not -e $from;
+	my $to = $h{$from};
+	print "uninstalling contents of $from from $to\n";
+	find(sub { 
+		 for ($File::Find::name) {
+#		     return if not -f; # why doesn't this work?
+		     s!^\Q$from\E/*!! or die;
+		     return if not length; # skip directory itself
+		     return if m!(?:/|^)\.exists!;
+		     my $inside_to = "$to/$_";
+		     if (-e $inside_to) {
+			 if (-f $inside_to) {
+			     print "unlinking $inside_to\n";
+			     unlink $inside_to or warn "cannot unlink $inside_to: $!";
+			 }
+			 elsif (-d $inside_to) {
+			     print "not removing directory $inside_to\n";
+			 }
+			 else {
+			     print "not removing special file $inside_to\n";
+			 }
+		     }
+		     else {
+			 print "$inside_to is not installed\n";
+		     }
+		 }
+	     }, $from);
+    }
+}
