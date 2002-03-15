610204e71eefbf50b118fa7ca575162be2ccd984
Ed Avis <ed@membled.com>
2002-03-15 18:54:04 +0000

    New version (though not a full rewrite) of the UK grabber.  This uses the new show=allchannels channel listing on the website, which eliminates a lot of the hassle with 'definitive' versus 'tentative' channel names, terrestrial channels not listed, and so on.  On the downside there is no longer any way to distinguish radio and satellite channels, the grabber guesses that based on the name.

--- grab/uk/Ananova_Channel.pm
+++ grab/uk/Ananova_Channel.pm
@@ -9,6 +9,7 @@ use Log::TraceMessages qw(t d);
 my @all;
 my %idx_a; # index by Ananova id
 my %idx_x; # index by XMLTV id
+my %known_regions;
 sub new {
     my $proto = shift;
     my $class = (ref $proto) || $proto;
@@ -33,28 +34,41 @@ sub new {
 # things like setting the display name based only on the first id, and
 # not again for all the other ids.
 #
+# Adding an Ananova id can also let you work out whether a channel is
+# terrestrial and what region it belongs to.
+#
 sub add_ananova_id {
     my $self = shift;
     my $id = shift;
     if (defined $idx_a{$id} and $idx_a{$id} ne $self) {
-	$self->croak("a channel with Ananova id $id already exists");
+	$self->carp("a channel with Ananova id $id already exists");
     }
     ++ $self->{ananova_ids}->{$id};
     $self->{first_ananova_id} = $id
       if not defined $self->{first_ananova_id};
     $idx_a{$id} = $self;
+
+    if ($id =~ /_(\d+)$/) {
+	$self->add_region($1);
+    }
+    else {
+	my $type = $self->get_type();
+	die "trying to add Ananova id $id, which has no region, to a terrestrial channel"
+	  if defined $type and $type eq 'terrestrial';
+    }
+
     return $self;
 }
 sub get_first_ananova_id {
     my $self = shift;
-    $self->croak('no Ananova ids set')
+    $self->carp('no Ananova ids set')
       if not defined $self->{first_ananova_id};
     return $self->{first_ananova_id};
 }
 sub is_ananova_id {
     my $self = shift;
     my $id = shift;
-    $self->croak('ids not set') if not defined $self->{ananova_ids};
+    $self->carp('ids not set') if not defined $self->{ananova_ids};
     return $self->{ananova_ids}->{$id};
 }
 # get_ananova_ids() not needed?
@@ -63,11 +77,11 @@ sub set_xmltv_id {
     my $self = shift;
     my $id = shift;
     for ($self->{xmltv_id}) {
-	$self->croak("cannot set XMLTV id to $id, already set to $_")
+	$self->carp("cannot set XMLTV id to $id, already set to $_")
 	  if defined and $_ ne $id;
     }
     if (defined $idx_x{$id} and $idx_x{$id} ne $self) {
-	$self->croak("a channel with XMLTV id $id already exists");
+	$self->carp("a channel with XMLTV id $id already exists");
     }
     $self->{xmltv_id} = $id;
     $idx_x{$id} = $self;
@@ -77,13 +91,7 @@ sub get_xmltv_id {
     my $self = shift;
 
     if (not defined $self->{xmltv_id}) {
-	# Invent an RFC2838-style name, if we have a definitive
-	# display name to make one from.  Otherwise undef.
-	#
-	if (not $self->has_definitive_display_name()) {
-	    return undef;
-	}
-
+	# Invent an RFC2838-style name.
 	my $display = $self->get_main_display_name();
 	die if not defined $display;
 	my $munged = $display;
@@ -95,8 +103,7 @@ sub get_xmltv_id {
 	my $new = "$munged.tv-listings.ananova.com";
 
 	# We just hope that the same name was not picked for some
-	# other channel.  It shouldn't be if we can trust display
-	# names to be different.
+	# other channel.
 	#
 	$self->set_xmltv_id($new);
     }
@@ -108,7 +115,7 @@ sub set_type {
     my $self = shift;
     my $type = shift;
     for ($self->{type}) {
-	$self->croak("cannot set type to $type, already set to $_")
+	$self->carp("cannot set type to $type, already set to $_")
 	  if defined and $_ ne $type;
     }
     $self->{type} = $type;
@@ -119,43 +126,73 @@ sub get_type {
     # Okay for type to be undef.
     return $self->{type};
 }
+sub guess_type {
+    my $self = shift;
+    for ($self->{type}) {
+	if (defined) {
+	    $self->carp("cannot guess type, already set to $_");
+	}
+	else {
+	    # If it were terrestrial this would have been set already
+	    # (when the Ananova id was set).  We can guess based on
+	    # the display name.
+	    #
+	    my $dn = $self->get_main_display_name();
+	    if (not defined $dn) {
+		$self->carp("cannot guess type, main display name not set");
+	    }
+	    else {
+		if ($dn =~ /\bradio\b/i or $dn =~ /\bFM\b/) {
+		    $_ = 'radio';
+		}
+		else {
+		    # Default.
+		    $_ = 'satellite';
+		}
+	    }
+	}
+	return $_;
+    }
+}
 
 # Similarly to Ananova id, region can be multivalued.
 sub add_region {
     my $self = shift;
     my $region = shift;
+    if (not $known_region{$region}++) {
+	$self->carp("unknown region $region");
+    }
+    $self->set_type('terrestrial');
     ++ $self->{regions}->{$region};
     return $self;
 }
 sub is_region {
     my $self = shift;
     my $region = shift;
-    $self->croak('regions not set') if not defined $self->{regions};
-    return $self->{regions}->{$region};
+    for ($self->{regions}) {
+	return $_->{$region} if defined;
+	return 0; # non-terrestrial chs belong to no region, okay.
+    }
 }
 # all_regions() not needed I think.
+sub know_regions {
+    shift; # discard package or object
+    foreach (@_) {
+	$known_region{$_}++
+	  && $self->carp("region $_ given twice to know_regions()");
+    }
+}
 
 sub set_main_display_name {
     my $self = shift;
-    my ($new_name, $defin) = @_;
-    if (not $defin) {
-	for ($self->{main_display_name}) {
-	    $self->croak("cannot set main display name to $new_name, already set to $_")
-	      if defined and $_ ne $new_name;
-	}
-	$self->{main_display_name} = $new_name;
-	$self->{main_display_name_defin} = 0;
-	return $self;
+    my $new_name = shift;
+    die if @_;
+    for ($self->{main_display_name}) {
+	$self->carp("cannot set main display name to $new_name, already set to $_")
+	  if defined and $_ ne $new_name;
     }
-    else {
-	for ($self->{main_display_name}) {
-	    $self->croak("cannot set main display name to $new_name, definitive name already set to $_")
-	      if $self->{main_display_name_defin} and $_ ne $new_name;
-	}
-	$self->{main_display_name} = $new_name;
-	$self->{main_display_name_defin} = 1;
-	return $self;
-    }
+    $self->{main_display_name} = $new_name;
+    return $self;
 }
 # Add additional display names to a channel.  This is an ordered list,
 # but duplicates are silently removed.
@@ -176,7 +213,7 @@ sub add_extra_display_names {
 sub get_display_names {
     my $self = shift;
     my $main = $self->{main_display_name};
-    $self->croak('main display name not set')
+    $self->carp('main display name not set')
       if not defined $main;
     my @r = ($main);
 
@@ -198,10 +235,6 @@ sub get_main_display_name {
     my $self = shift;
     # Okay to return undef.
     return $self->{main_display_name};
-}
-sub has_definitive_display_name {
-    my $self = shift;
-    return (defined $self->{main_display_name}) && $self->{main_display_name_defin};
 }
 # Get some kind of display name to show to the user in error messages.
 sub get_a_display_name {
