baecbbb77ff99831754dee8684002e3594673170
Robert Eden <rmeden@yahoo.com>
2003-06-01 05:46:13 +0000

    Initial Checkin. This scipt is used to seed the PERLAPP options for for use in making xmltv.exe It's pretty much constants except for the version fields.

---
+++ lib/exe_opt.pl
@@ -0,0 +1,36 @@
+#!perl -w
+#
+# $Id$
+#
+# This is a simple script to generate options so PerlApp can make the EXE
+# it needs time values, so might as well put it in a perl script!
+# (windows has a limited date function)
+#
+# Robert Eden rmeden@yahoo.com
+#
+
+#
+# output constants
+#
+print '-nologo
+-force
+-trim="Convert::EBCDIC;DB_File;Encode;HASH;HTML::FromText;Text::Iconv;Unicode::Map8;v5"
+-info CompanyName="XMLTV Project http://membled.com/work/apps/xmltv/"
+-info FileDescription="EXE bundle of XMLTV tools to manage TV Listings"
+-info InternalName=xmltv.exe
+-info OriginalFilename=xmltv.exe
+-info ProductName=xmlTV
+-info LegalCopyright="GNU General Public License http://www.gnu.org/licenses/gpl.txt"
+';
+
+#
+# put date in file version field
+#
+@date=localtime; $date[4]++; $date[5]+=1900;
+printf "-info FileVersion=%4d.%d.%d.%d\n",@date[5,4,3,2];
+
+#
+# last fields in product version should ommitable, but it doesn't work.
+#
+$version=shift;
+printf "-info ProductVersion=%d.%d.%d.%d\n",split(/\./,$version);
