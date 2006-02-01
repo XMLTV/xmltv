bfb2561dfdbe6981a52c002318c6caf38cd73637
Mattias Holmlund <u82@m1.holmlund.se>
2006-02-01 18:46:24 +0000

    Print a summary at the end of test_grabbers. Check that the --days and --offset options are honoured. Document error-codes.

--- lib/ValidateFile.pm
+++ lib/ValidateFile.pm
@@ -1,5 +1,7 @@
 package XMLTV::ValidateFile;
 
+use strict;
+
 BEGIN {
     use Exporter   ();
     our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
@@ -34,8 +36,6 @@ All these functions are exported on demand.
 
 =cut
 
-sub w;
-
 =item LoadDtd
 
 Load the xmltv dtd. Takes a single parameter which is the name of
@@ -45,8 +45,7 @@ LoadDtd must be called before ValidateFile can be called.
 
 =cut
 
-sub LoadDtd
-{ 
+sub LoadDtd { 
     my( $dtd_file ) = @_;
 
     my $dtd_str = read_file($dtd_file) 
@@ -63,8 +62,9 @@ sub LoadDtd
 
 Validate that a file is valid according to the XMLTV dtd and try to check
 that it contains valid information. ValidateFile takes a filename as parameter
-and returns the number of errors found in the file. Error messages are printed
-to STDERR.
+and optionally also a day and an offset and returns an array of all the types
+of errors found in the file. If no errors were found, an empty array is 
+returned. Error messages are printed to STDERR.
 
 ValidateFile checks the following:
 
@@ -72,150 +72,190 @@ ValidateFile checks the following:
 
 =item *
 
-File is well-formed XML.
+File is well-formed XML (notwell).
 
 =item *
 
-File follows the XMLTV DTD.
+File follows the XMLTV DTD (notdtd).
 
 =item *
 
 There is exactly one channel-entry for each channel mentioned in a 
-programme-entry.
+programme-entry (unknownid, duplicatechannel).
+
+=item *
+
+There are programme entries for all channel-entries (channelnoprogramme).
 
 =item *
 
 All xmltvids look like proper ids, i.e. they match 
-/^[-a-zA-Z0-9]+(\.[-a-zA-Z0-9]+)+$/.
+/^[-a-zA-Z0-9]+(\.[-a-zA-Z0-9]+)+$/ (invalidid).
 
 =item *
 
-Each programme entry has a valid channel id.
+Each programme entry has a valid channel id (noid).
 
 =item *
 
-Each programme entry has a non-empty title.
+Each programme entry has a non-empty title (emptytitle).
 
 =item *
 
-Each programme entry has a valid start-time. 
+Each programme entry has a valid start-time (badstart). 
 
 =item *
 
-If a programme has a stop-time, it must be valid.  
+If a programme has a stop-time, it must be valid (badstop).
+
+=item *
+
+If the day and offset parameters were specified in the call to ValidateFile,
+it checks to see that the start-time of each program is between 
+today+$days 00:00 and today+$offset+$days+1 06:00. This check does not take
+timezones into account, it ígnores the timezone component and assumes that
+the start-time is expressed in local time (earlystart, latestart).
 
 =back
 
 =cut 
+
+my %errors;
   
-sub ValidateFile
-{
-    my( $file ) = @_;
+sub ValidateFile {
+    my( $file, $offset, $days ) = @_;
 
     die "ValidateFile called without previous call to LoadDtd" 
 	unless defined $dtd;
 
-    my $errors = 0;
-    
+    my $mintime = "19700101000000";
+    my $maxtime = "29991231235959";
+
+    my $minerr = 0;
+    my $maxerr = 0;
+
+    if (defined $offset) {
+	require DateTime;
+	$mintime = DateTime->now->add( days => $offset )->ymd("") . "000000";
+    }
+
+    if (defined $days ) {
+	$maxtime = DateTime->now->add( days => $offset+$days )->ymd("") .
+	    "060000";
+    }
+
+    %errors = ();
+
     my $doc;
     
     eval { $doc = $parser->parse_file( $file ); };
     
-    if ( $@ )
-    {
-	w "The file is not well-formed xml:\n$@ ";
-	return 1;
+    if ( $@ ) {
+	w( "The file is not well-formed xml:\n$@ ", 'notwell');
+	return (keys %errors);
     }
     
     eval { $doc->validate( $dtd ) };  
-    if ( $@ )
-    {
-	w "The file is not valid according to the xmltv dtd:\n $@";
-	return 1;
+    if ( $@ ) {
+	w( "The file is not valid according to the xmltv dtd:\n $@",
+	   'notvalid' );
+	return (keys %errors);
     }
 
-    my $w = sub 
-    { 
-	w "Line " . $_[0]->line_number() . " $_[1]";
-	$errors++;
+    my $w = sub { 
+	my( $p, $msg, $id ) = @_;
+	w( "Line " . $p->line_number() . " $msg", $id );
     };
     
     my %channels;
     
     my $ns = $doc->find( "//channel" );
-    if( $ns->size() == 0 )
-    {
-	w "No channel entries found.";
-	$errors++;
-    }
 
-    foreach my $ch ($ns->get_nodelist)
-    {
+    foreach my $ch ($ns->get_nodelist) {
 	my $channelid = $ch->findvalue('@id');
 	my $display_name = $ch->findvalue('display-name/text()');
 	
-	
-	$w->( $ch, "Illegal channel-id $channelid" )
+	$w->( $ch, "Invalid channel-id '$channelid'", 'invalidid' )
 	    if $channelid !~ /^[-a-zA-Z0-9]+(\.[-a-zA-Z0-9]+)+$/;
 	
-	$w->( $ch, "Duplicate channel-tag for $channelid" )
+	$w->( $ch, "Duplicate channel-tag for '$channelid'", 'duplicateid' )
 	    if defined( $channels{$channelid} );
 	
 	$channels{$channelid} = 0;
     }
     
     $ns = $doc->find( "//programme" );
+    if ($ns->size() == 0) {
+	w( "No programme entries found.", 'noprogrammes' );
+	return (keys %errors);
+    }
 
-    foreach my $p ($ns->get_nodelist)
-    {
+    foreach my $p ($ns->get_nodelist) {
 	my $channelid = $p->findvalue('@channel');
 	my $start = $p->findvalue('@start');
 	my $stop = $p->findvalue('@stop');
 	my $title = $p->findvalue('title/text()');
 	my $desc = $p->findvalue('desc/text()');
 		
-	if ( not defined( $channels{$channelid} ))
-	{
-	    $w->( $p, "Channel $channelid does not have a <channel>-entry." );
+	if (not defined( $channels{$channelid} )) {
+	    $w->( $p, "Channel '$channelid' does not have a <channel>-entry.",
+		  'unknownid' );
 	    $channels{$channelid} = "auto";
-	    $errors++;
 	}
 
 	$channels{$channelid}++;
 	
-	$w->( $p, "Empty title" )    
+	$w->( $p, "Empty title", 'emptytitle' )    
 	    if $title =~ /^\s*$/;
 	
-	$w->( $p, "Illegal start-time $start" )
+	$w->( $p, "Invalid start-time '$start'", 'badstart' )
 	    if not verify_time( $start );
 	
-	$w->( $p, "Illegal stop-time $stop" )
+	$w->( $p, "Invalid stop-time '$stop'", 'badstop' )
 	    if $stop ne "" and not verify_time( $stop );
-    }
 
-    foreach my $channel (keys %channels)
-    {
-	if( $channels{$channel} == 0 )
-	{
-	    w "No programme entries found for $channel";
-	    $errors++;
+	if ($start lt $mintime) {
+	    $w->( $p, "Start-time too early $start", 'earlystart' ) 
+		unless $minerr;
+	    $minerr++;
+	}
+
+	if ($start gt $maxtime) {
+	    $w->( $p, "Start-time too late $start", 'latestart' )
+		unless $maxerr;
+	    $maxerr++;
 	}
     }
 
-    return $errors;
+    foreach my $channel (keys %channels) {
+	if ($channels{$channel} == 0) {
+	    w( "No programme entries found for $channel", 
+	       'channelnoprogramme' );
+	}
+    }
+    
+    w( "$minerr entries had a start-time earlier than $mintime" )
+	if $minerr;
+
+    w( "$maxerr entries had a start-time later than $maxtime" )
+	if $maxerr;
+    
+    return (keys %errors);
 }
 
 sub verify_time
 {
     my( $time ) = @_;
 
-    return $time =~ /^\d{12,14}(\s+([A-Z]+|[+-]\d{4}))$/;
+    return $time =~ /^\d{12,14}(\s+([A-Z]+|[+-]\d{4})){0,1}$/;
 }
 
-sub w
-{
-    print "$_[0]\n";
+sub w {
+    my( $msg, $id ) = @_;
+    print "$msg\n";
+    $errors{$id}++ if defined $id;
 }
+    
 
 1;
 
