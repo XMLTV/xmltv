dbe11d76d0b11e8bdfc91542a5d686dbaca79309
Robert Eden <rmeden@yahoo.com>
2007-10-01 13:27:16 +0000

    prepare for 0.5.49 release

--- lib/exe_wrap.pl
+++ lib/exe_wrap.pl
@@ -69,7 +69,7 @@ print STDERR "Timezone is $ENV{TZ}\n" unless $opt_quiet;
 $cmd = shift || "";
 
 # --version (and abbreviations thereof)
-my $VERSION = '0.5.48';
+my $VERSION = '0.5.49';
 if (index('--version', $cmd) == 0 and length $cmd >= 3) {
     print "xmltv $VERSION\n";
     exit;
