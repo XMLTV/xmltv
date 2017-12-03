c2bc10c440c9b997374fb5ab4167a824d46fb06f
Stefan Becker <chemobejk@gmail.com>
2017-12-03 14:41:01 +0200

    fi: update fetch script for GitHub
    Unfortunately GitHub does not offer tarballs for directories, just
    repositories.

--- grab/fi/get_latest_version.sh
+++ grab/fi/get_latest_version.sh
@@ -1,17 +1,32 @@
 #/bin/sh
 set -e
 
-# Fetch tarball from sourceforge CVS web viewer
-echo "Fetching latest tv_grab_fi code..."
-wget --quiet --clobber -O tv_grab_fi.tgz \
-	'http://xmltv.cvs.sourceforge.net/viewvc/xmltv/xmltv/grab/fi/?view=tar'
+# GitHub does not offer tarballs of directories :-(
+files=(
+    merge.PL
+    tv_grab_fi.pl
+    fi/common.pm
+    fi/day.pm
+    fi/programme.pm
+    fi/programmeStartOnly.pm
+    fi/source/foxtv.pm
+    fi/source/iltapulu.pm
+    fi/source/telkku.pm
+    fi/source/yle.pm
+)
 
-# Unpack
-echo "Unpacking tarball..."
-rm -rf fi tv_grab_fi
-tar xfz tv_grab_fi.tgz
+# Fetch files from GitHub repository
+echo "Fetching latest tv_grab_fi code..."
+rm -rf fi
+for _f in ${files[@]}; do
+    echo "Fetching ${_f}..."
+    mkdir -p $(dirname fi/${_f})
+    wget --quiet --clobber -O fi/${_f} \
+	 "https://raw.githubusercontent.com/XMLTV/xmltv/master/grab/fi/${_f}"
+done
 
 # Generate tv_grab_fi
+rm -f tv_grab_fi
 perl fi/merge.PL tv_grab_fi
 rm -rf fi
 ls -l tv_grab_fi*
