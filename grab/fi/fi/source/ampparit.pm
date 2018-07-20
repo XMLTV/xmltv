679a9a6af123db8e59017ec6714a4a19a09257bb
Stefan Becker <chemobejk@gmail.com>
2018-07-20 14:39:38 +0300

    ampparit: implement channel() method

--- grab/fi/fi/source/ampparit.pm
+++ grab/fi/fi/source/ampparit.pm
@@ -17,12 +17,76 @@ BEGIN {
   our $ENABLED = 1;
 }
 
+# Import from internal modules
+fi::common->import();
+
 # Description
 sub description { 'ampparit.com' }
 
 # Grab channel list
 sub channels {
-  return;
+  my %channels;
+
+  # Fetch & parse HTML (do not ignore HTML5 <time>)
+  my $root = fetchTree("https://www.ampparit.com/tv/");
+
+  if ($root) {
+
+    #
+    # Channel list can be found from this list:
+    #
+    #   <div class="programming-container">
+    #    ...
+    #	 <div class="channel col-xs-6 col-sm-4 col-md-3 col-lg-3">
+    #     <div class="channel-wrap">
+    #      <div class="channel-header">
+    #       <a class="logo" href="/tv/yle-tv1">
+    #        <img src="..." alt="Yle TV1">
+    #       </a>
+    #       ...
+    #      </div>
+    #     </div>
+    #     <div class="programs">...</div>
+    #    </div>
+    #    ,,,
+    #   </div>
+    #
+    if (my $container = $root->look_down("class" => "programming-container")) {
+      if (my @links = $container->look_down("_tag"  => "a",
+					    "class" => qr/^(?:logo|name)$/)) {
+	foreach my $link (@links) {
+	  my $id = $link->attr("href");
+
+	  # strip path
+	  $id = (split("/", $id))[-1];
+
+	  my $name;
+	  if (my $img = $link->find("img")) {
+	    $name = $img->attr("alt");
+	  } else {
+	    # no logo
+	    $name = $link->as_text();
+	  }
+	  $name =~ s/\s+$//;
+
+	  if (defined($id)   && length($id) &&
+	      defined($name) && length($name)) {
+	    debug(3, "channel '$name' ($id)");
+	    $channels{"${id}.ampparit.com"} = "fi $name";
+	  }
+	}
+      }
+    }
+
+    # Done with the HTML tree
+    $root->delete();
+
+  } else {
+    return;
+  }
+
+  debug(2, "Source ampparit.com parsed " . scalar(keys %channels) . " channels");
+  return(\%channels);
 }
 
 # Grab one day
