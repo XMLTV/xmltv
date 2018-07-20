6eade4dcc2e595d55bb4cf8d3a109d0ca8c0d4d3
Stefan Becker <chemobejk@gmail.com>
2018-07-20 19:35:27 +0300

    telsu: implement grab() method

--- grab/fi/fi/source/telsu.pm
+++ grab/fi/fi/source/telsu.pm
@@ -75,6 +75,86 @@ sub channels {
 
 # Grab one day
 sub grab {
+  my($self, $id, $yesterday, $today, $tomorrow, $offset) = @_;
+
+  # Get channel number from XMLTV id
+  return unless my($channel) = ($id =~ /^([^.]+)\.telsu\.fi$/);
+
+  # Fetch & parse HTML
+  my $root = fetchTree("https://www.telsu.fi/" . $today->ymd() . "/$channel");
+  if ($root) {
+    my @objects;
+
+    #
+    # Each programme can be found in a separate <div class="dets stat"> node
+    #
+    #   <div class="dets stat" rel="...">
+    #    <div class="c">
+    #     <div class="h">
+    #      <h1>
+    #       <b>Uutisikkuna</b>
+    #       <em class="k0" title="Ohjelma on sallittu kaikenikäisille.">S</em>
+    #      </h1>
+    #      <h2>
+    #       <i>ma 24.07.2017</i> 04:00 - 06:50 <img src="...">
+    #       <div class="rate" ...>...</div>
+    #      </h2>
+    #     </div>
+    #     <div class="t">
+    #      <div>Uutisikkuna</div>
+    #     </div>
+    #     ...
+    #    </div>
+    #   </div>
+    #
+    if (my @programmes = $root->look_down("class" => "dets stat")) {
+      my @offsets = ($yesterday, $today, $tomorrow);
+      my $current = ''; # never matches -> $yesterday will be removed first
+
+      foreach my $programme (@programmes) {
+	my $title = $programme->find("b");
+	my $time  = $programme->find("h2");
+	my $desc  = $programme->look_down("class" => "t");
+
+	if ($title && $time && $desc) {
+	  if (my($new, $start_h, $start_m, $end_h, $end_m) =
+	      $time->as_text() =~ /^(.+)\s(\d{2}):(\d{2})\s-\s(\d{2}):(\d{2})/) {
+	    $title = $title->as_text();
+	    $desc  = $desc->as_text();
+
+	    # Detect day change
+	    if ($new ne $current) {
+	      $current = $new;
+	      shift(@offsets);
+	    }
+	    my $start = timeToEpoch($offsets[0], $start_h, $start_m);
+	    my $end   = timeToEpoch($offsets[0], $end_h,   $end_m);
+
+	    # Detect end time on next day
+	    $end = timeToEpoch($offsets[1], $end_h, $end_m)
+	      if ($end < $start);
+
+	    debug(3, "List entry ${id} ($start -> $end) $title");
+	    debug(4, $desc) if $desc;
+
+	    # Create program object
+	    my $object = fi::programme->new($id, "fi", $title, $start, $end);
+	    $object->description($desc);
+	    push(@objects, $object);
+	  }
+	}
+      }
+    }
+
+    # Done with the HTML tree
+    $root->delete();
+
+    # Fix overlapping programmes
+    fi::programme->fixOverlaps(\@objects);
+
+    return(\@objects);
+  }
+
   return;
 }
 
