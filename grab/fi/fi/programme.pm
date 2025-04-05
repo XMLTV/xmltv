79a7037eececa04e3d82387a97aacb499ada5128
Stefan Becker <chemobejk@gmail.com>
2025-04-05 19:12:56 +0300

    programme: add parser for ^K<N>, J<N>: ...
    Iltapulu switched to this new description format with season & episode
    number.
    
    Fixes #263

--- grab/fi/fi/programme.pm
+++ grab/fi/fi/programme.pm
@@ -271,6 +271,15 @@ sub dump {
 	# Repeat the above match on remaining description
 	($left, $special, $right) = ($remainder =~ $match_description);
 
+    # Check for "K<season>, J<episode: <sub-title>...."
+    } elsif (($desc_season, $desc_episode, $remainder) =
+	     ($description =~ m/^K(\d+),\s+J(\d+):\s+(.*)$/)) {
+	$season  = $desc_season;
+	$episode = $desc_episode;
+
+	# Repeat the above match on remaining description
+	($left, $special, $right) = ($remainder =~ $match_description);
+
     # Check for "<sub-title>. Kausi <season>, (jakso )?<episode>/<# of episodes>...."
     } elsif (($desc_subtitle, $desc_season, $desc_episode, $desc_total, $remainder) =
 	     ($description =~ m!^(.+)\s+Kausi\s+(\d+),\s+(?:jakso\s+)?(\d+)(?:/(\d+))?\.\s*(.*)$!)) {
