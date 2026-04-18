94b5da34ac13ab0d7f8db29b22da8b040c016bcc
Stefan Becker <chemobejk@gmail.com>
2026-04-18 22:17:52 +0300

    programme: fix broken sub-title implementation
    This fixes commit ceddb4300a4596d6f06d856e100003fcd1884f3c
    
    Key "sub-title" must be an array ref. Convert the SxEy fallback
    implementation to a sub-title without language specifier.
    
    I wonder if the original author has ever tested his change, because at
    least in my test environment the grabber crashes immediately.
    
    - fix incorrect whitespace
    - use the same debug approach as the other code path

--- grab/fi/fi/programme.pm
+++ grab/fi/fi/programme.pm
@@ -337,8 +337,9 @@ sub dump {
     debug(3, "XMLTV programme episode ($_->[1]): $_->[0]")
       foreach (@{ $xmltv{'sub-title'} });
   } elsif (defined($season) && defined($episode)) {
-  	$xmltv{'sub-title'} = 'S' . $season . 'E' . $episode;
-    debug(3, "XMLTV programme subtitle: S${season}E${episode}");
+    $xmltv{'sub-title'} = [["S${season}E${episode}"]];
+    debug(3, "XMLTV programme episode: $_->[0]")
+      foreach (@{ $xmltv{'sub-title'} });
   }
   if (defined($category) && length($category)) {
     $xmltv{category} = [[$category, $language]];
